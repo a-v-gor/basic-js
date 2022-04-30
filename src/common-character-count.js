f19666e52076422b4d44b44f5b871962a366031b
Alexey Gorbenko <a.v.gor@mail.ru>
2022-04-30 17:41:15 +0300

    feat: add decision for extended-repeater

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -11,9 +11,11 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getCommonCharacterCount(s1, s2) {
+  const arr1 = s1.split('');
+  const arr2 = s2.split('');
+  let result = new Set();
+  arr1.forEach((item) => arr2.includes(item) ? result.add(item) : );
 }
 
 module.exports = {

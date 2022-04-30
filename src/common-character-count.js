84e33cea2ded54b3b674e185998805c000950d29
Alexey Gorbenko <a.v.gor@mail.ru>
2022-04-30 19:55:05 +0300

    feat: add decision for common-character-count

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -11,11 +11,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(s1, s2) {
+ function getCommonCharacterCount(s1, s2) {
   const arr1 = s1.split('');
   const arr2 = s2.split('');
-  let result = new Set();
-  arr1.forEach((item) => arr2.includes(item) ? result.add(item) : );
+  let result = [];
+  for (let i = 0; i< arr1.length; i++) {
+    let item = arr1[i];
+    if (arr2.includes(item)) {
+      result.push(item);
+      arr1.splice(i,1);
+      const index2 = arr2.indexOf(item);
+      arr2.splice(index2,1);
+      i--;
+    }
+  }
+  return result.length;
 }
 
 module.exports = {

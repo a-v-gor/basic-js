7488d81ac2a97913a577a463a6f9fe31098be6d4
Alexey Gorbenko <a.v.gor@mail.ru>
2022-05-02 07:07:49 +0300

    feat: make decision for sum-digits task

--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -11,9 +11,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function sortByHeight(arr) {
+  let intHeightsArr = [];
+  let rezArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] >= 0) {
+      intHeightsArr.push(arr[i]);
+    }
+  }
+  intHeightsArr.sort((a, b) => b - a);
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] == -1) {
+      rezArr.push(arr[i])
+    } else {
+      rezArr.push(intHeightsArr.pop())
+    }
+  }
+  return rezArr;
 }
 
 module.exports = {
